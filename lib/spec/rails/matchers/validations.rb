3f4897f7702efdc499b3d0ebcadfdada2c854bee
nodanaonlyzuul <stephen@eastmedia.com>
2010-10-04 14:52:20 -0400

    Updated to new matcher syntax http://github.com/dchelimsky/rspec/wiki/custom-matchers

--- lib/spec/rails/matchers/validations.rb
+++ lib/spec/rails/matchers/validations.rb
@@ -1,92 +1,76 @@
-module Spec
-  module Rails
-    module Matchers
-      def validate_presence_of(attribute)
-        return simple_matcher("model to validate the presence of #{attribute}") do |model|
-          if !model.respond_to?("#{attribute}=")
-            false
-          else
-            model.send("#{attribute}=", nil)
-            !model.valid? && model.errors.invalid?(attribute)
-          end
-        end
-      end
+RSpec::Matchers.define :validate_presence_of do |attribute|
+  match do |object|
+    if !object.respond_to?("#{attribute}=")
+      false
+    else
+      object.send("#{attribute}=", nil)
+      !object.valid? && object.errors[attribute].any?
+    end
+  end
+end
 
-      def validate_length_of(attribute, options)
-        if options.has_key? :within
-          min = options[:within].first
-          max = options[:within].last
-        elsif options.has_key? :is
-          min = options[:is]
-          max = min
-        elsif options.has_key? :minimum
-          min = options[:minimum]
-        elsif options.has_key? :maximum
-          max = options[:maximum]
-        end
+RSpec::Matchers.define :validate_length_of do |attribute, options|
+  if options.has_key? :within
+    min = options[:within].first
+    max = options[:within].last
+  elsif options.has_key? :is
+    min = options[:is]
+    max = min
+  elsif options.has_key? :minimum
+    min = options[:minimum]
+  elsif options.has_key? :maximum
+    max = options[:maximum]
+  end
 
-        return simple_matcher("model to validate the length of #{attribute} within #{min || 0} and #{max || 'Infinity'}") do |model|
-          invalid = false
-          if !min.nil? && min >= 1
-            model.send("#{attribute}=", 'a' * (min - 1))
+  invalid = false
+  if !min.nil? && min >= 1
+    object.send("#{attribute}=", 'a' * (min - 1))
+    invalid = !object.valid? && object.errors[attribute].any?
+  end
 
-            invalid = !model.valid? && model.errors.invalid?(attribute)
-          end
+  if !max.nil?
+    object.send("#{attribute}=", 'a' * (max + 1))
+    invalid ||= !object.valid? && object.errors[attribute].any?
+  end
 
-          if !max.nil?
-            model.send("#{attribute}=", 'a' * (max + 1))
+  invalid
+end
 
-            invalid ||= !model.valid? && model.errors.invalid?(attribute)
-          end
-          invalid
-        end
-      end
+RSpec::Matchers.define :validate_uniqueness_of do |attribute|
+  match do |object|
+    object.class.stub!(:with_exclusive_scope).and_return([object])
+    !object.valid? && object.errors[attribute].any?
+  end
+end
 
-      def validate_uniqueness_of(attribute)
-        return simple_matcher("model to validate the uniqueness of #{attribute}") do |model|
-          model.class.stub!(:with_exclusive_scope).and_return([model])
-          !model.valid? && model.errors.invalid?(attribute)
-        end
-      end
+RSpec::Matchers.define :validate_confirmation_of do |attribute|
+  match do |object|
+    object.send("#{attribute}_confirmation=", 'asdf')
+    !object.valid? && object.errors[attribute].any?
+  end
+end
 
-      def validate_confirmation_of(attribute)
-        return simple_matcher("model to validate the confirmation of #{attribute}") do |model|
-          model.send("#{attribute}_confirmation=", 'asdf')
-          !model.valid? && model.errors.invalid?(attribute)
-        end
-      end
+RSpec::Matchers.define :validate_inclusion_of do |attribute, options|
+  match do |object|
+    values = options[:in]
+    booleans = assign_and_validate_values(object, attribute, values)
+    booleans.all?
+  end
+end
 
-      def validate_inclusion_of(attribute, hash)
-        values = hash[:in]
+module RSpec
+  module Matchers
 
-        simple_matcher("model to validate the inclusion of #{attribute} in #{values.inspect}") do |model|
-          booleans = assign_and_validate_values(model, attribute, values)
-          booleans.all?
-        end
-      end
+  private
+    def assign_and_validate_value(model, attribute, value)
+      model.send("#{attribute}=", value)
+      model.valid?
+      !model.errors.invalid?(attribute)
+    end
 
-      def validate_boolean_of(attribute)
-        values = [true, false]
-
-        simple_matcher("model to validate boolean of #{attribute}") do |model|
-          booleans = assign_and_validate_values(model, attribute, [true, false])
-          booleans << !assign_and_validate_value(model, attribute, nil)
-          booleans.all?
-        end
-      end
-
-    private
-
-      def assign_and_validate_value(model, attribute, value)
-        model.send("#{attribute}=", value)
-        model.valid?
-        !model.errors.invalid?(attribute)
-      end
-
-      def assign_and_validate_values(model, attribute, values)
-        values.map do |value|
-          assign_and_validate_value(model, attribute, value)
-        end
+    def assign_and_validate_values(model, attribute, values)
+      values.map do |value|
+        assign_and_validate_value(model, attribute, value)
       end
     end
   end
